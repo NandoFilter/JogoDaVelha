1e26c827f82a01aff9aba5f5324966ea96d6636e
NandoFilter <fernandohfilter10@gmail.com>
2021-09-12 11:47:52 -0300

    Organização dos arquivos

---
+++ files/Jogador.java
@@ -0,0 +1,59 @@
+package jogo_da_velha.files;
+
+import java.util.Scanner;
+
+public class Jogador {
+    
+    String[][] plano;
+    String[][] jogadaAnterior;
+    String simbolo;
+    Scanner scanner;
+
+    public Jogador (String[][] plano) {
+        this.plano = plano;
+        this.jogadaAnterior = plano;
+    }
+
+    public void setSimbolo(String simbolo) {
+        this.simbolo = simbolo;
+    }
+
+    public String getSimbolo() {
+        return this.simbolo;
+    }
+
+    public boolean jogar() {
+
+        scanner = new Scanner(System.in);
+
+        System.out.println("-----------------");
+
+        System.out.print("Linha: ");
+        int linha = scanner.nextInt();
+
+        System.out.print("Coluna: ");
+        int coluna = scanner.nextInt();
+
+        linha--;
+        coluna--;
+
+        if ( jogadaAnterior[linha][coluna].equals(" ") ) {
+
+            plano[linha][coluna] = getSimbolo();
+            jogadaAnterior[linha][coluna] = getSimbolo();
+
+            System.out.println();
+            return true;
+
+        } else {
+
+            System.out.println("Posição já preenchida!");
+
+            System.out.println();
+            return false;
+
+        }
+            
+    }
+
+}
---
+++ files/Main.java
@@ -0,0 +1,14 @@
+package jogo_da_velha.files;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main (String[] args) throws InterruptedException, IOException {
+
+        String[][] plano = new String[3][3];
+        JogoDaVelha jogo = new JogoDaVelha(plano);
+
+        jogo.jogar();
+        
+    }
+}
